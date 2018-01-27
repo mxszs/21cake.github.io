99cf6b601d4d4fb2ed2a01d997aa86f28d0c237e
mxszs <1344993660@qq.com>
2018-01-27 09:56:07 +0800

    构建个人页面

--- src/javascripts/components/mine/Mine_info.js
+++ src/javascripts/components/mine/Mine_info.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import {connect} from 'react-redux'
 class   MineInfo extends React.Component{
         constructor(props){
             super(props)
@@ -11,7 +12,28 @@ class   MineInfo extends React.Component{
             return(
                 <div className ="account-area">
                       <div className="edit_info">
-
+                            <div className="edit_iofo_left">
+                                <span className='change_head'> <span className='change_head_mask'></span></span>
+                               
+                            </div>
+                            <div className="edit_info_right">
+                                <h2 className='phone-info'>{this.props.User.userLoginInfo.phone}</h2>
+                                <p>{this.props.User.userLoginInfo.phone} &nbsp;&nbsp;<span>修改密码</span></p>
+                                <ul>
+                                    <li className='clear'>
+                                         <i className='edit_ico person'></i>  <span>编辑个人资料</span>
+                                     
+                                    </li>
+                                    <li className='clear'>
+                                        <i className='edit_ico address'></i>   <span>地址管理</span>
+                                      
+                                    </li>
+                                    <li className='clear'>
+                                         <i className='edit_ico buy'></i> <span>订单管理</span>
+                                       
+                                    </li>
+                                </ul>
+                            </div>
                       </div>
                       <div className="cash_coupou">
 
@@ -21,4 +43,4 @@ class   MineInfo extends React.Component{
         }
 
 }
-export default MineInfo
+export default connect(state=>state)(MineInfo)
