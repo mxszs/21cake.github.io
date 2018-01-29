8b3eb60948fcfad57eda92ebe39b5453967f1022
mxszs <1344993660@qq.com>
2018-01-29 19:31:37 +0800

    改了主页

--- src/javascripts/components/home/Home.js
+++ src/javascripts/components/home/Home.js
@@ -1,6 +1,9 @@
 
 import React from 'react'
 import Header from '../header'
+import Banner from './Banner'
+import Content from './Content'
+import Footer from '../footer/Footer'
 class Home extends React.Component{
     constructor(props){
         super(props)
